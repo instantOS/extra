429284fd65d8842100d7e83abda11fe043a2f565
paperbenni <paperbenni@gmail.com>
2020-01-26 19:15:32 +0100

    fix package name

--- build.sh
+++ build.sh
@@ -4,10 +4,19 @@ mac
 arc
 manjaro"
 
+[ -e build ] && rm -rf build
 mkdir build
+
 cd instantwm
 
-for i in "$THEMES"; do
+if [ -e src ]; then
+    rm -rf instantwm-git
+    rm -rf src
+    rm -rf pkg
+    rm *.pkg.tar.xz
+fi
+
+for i in $THEMES; do
     echo "$i" >/tmp/instanttheme
     makepkg .
     mv *.pkg.tar.xz ../build/instantwm-$i.pkg.tar.xz
