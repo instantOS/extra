eef8ab7a5a905df2f9d2c026ab374a25a297004b
paperbenni <paperbenni@gmail.com>
2020-02-23 16:03:59 +0100

    add meta packages

--- build.sh
+++ build.sh
@@ -2,27 +2,36 @@
 ############################################################
 ## build all instantos packages to recreate a full mirror ##
 ############################################################
-
+echo "starting instantOS repo build"
 # build functions
 source utils.sh
 
+# themable instantOS packages
 themebuild instantwm
 themebuild instantmenu
 themebuild instantlock
 
-bashbuild instantassist
-bashbuild instantwallpaper
-bashbuild instantutils
+# third party packages not available in the default repos
 bashbuild xdragon
-bashbuild grub-theme-live
+# bashbuild grub-theme-live // not needed at the time
 bashbuild instantdotfiles
 bashbuild liveutils
 bashbuild paperbash
+
+# instantOS exclusive packages
+bashbuild instantassist
+bashbuild instantwallpaper
+bashbuild instantutils
 bashbuild instantthemes
 bashbuild instantwidgets
 bashbuild instantcursors
 bashbuild instantshell
 
+# meta packages
+bashbuild instantos
+bashbuild instantdepend
+
+# aur packages
 aurbuild wmutils-git wmutils
 aurbuild libinput-gestures
 aurbuild autojump
@@ -34,3 +43,4 @@ linkbuild panther_launcher https://www.rastersoft.com/descargas/panther_launcher
 
 repo-add instant.db.tar.xz ./*.pkg.tar.xz
 apindex .
+echo "done building repos"
