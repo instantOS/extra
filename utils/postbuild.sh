e3c300f6238220bfd652c06e09fc3452fc24bda3
paperbenni <paperbenni@gmail.com>
2020-03-24 17:20:22 +0100

    fix postbuild

--- utils/postbuild.sh
+++ utils/postbuild.sh
@@ -9,16 +9,17 @@ cd ~/stuff/extra/build || exit 1
 
 repo-add instant.db.tar.xz ./*.pkg.tar.xz
 [ -e index.html ] && rm index.html
-apindex . || echo "error: apindex not found" && exit
+
+if ! apindex .; then
+    echo "error: apindex not found"
+    exit 1
+fi
+
 echo "done building repos"
 
-if [ -e index.html ]; then
-    if [ -z "$1" ]; then
-        surge . instantos.surge.sh
-    else
-        surge . instantos32.surge.sh
-    fi
+echo "uploading to surge"
+if [ -z "$1" ]; then
+    surge . instantos.surge.sh
 else
-    echo "repo build failed"
-    exit 1
+    surge . instantos32.surge.sh
 fi
