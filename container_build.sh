1ca48b731e06ac1090956792339e944db552fd7b
paperbenni <paperbenni@gmail.com>
2025-11-27 13:07:43 +0100

    add local repo

--- container_build.sh
+++ container_build.sh
@@ -3,7 +3,17 @@ set -e
 
 # This script runs INSIDE the container
 
+# 1. Configure local repo if it exists
+# This allows packages to depend on previously built packages
+if [ -f /repo/instant.db.tar.gz ]; then
+    echo "Adding local repo to pacman.conf"
+    echo "[instant]" >> /etc/pacman.conf
+    echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
+    echo "Server = file:///repo" >> /etc/pacman.conf
+fi
+
 # Install dependencies
+# We sync (-y) to pick up the local repo if added
 pacman -Syu --noconfirm --needed base-devel git sudo
 
 # Create a builder user
@@ -21,3 +31,8 @@ sudo -u builder makepkg -s --noconfirm
 # Move built packages to the repo mount
 # We assume the repo is mounted at /repo
 mv *.pkg.tar.zst /repo/
+
+# Update the repository database so subsequent builds can find this package
+cd /repo
+# We add the just-built packages to the DB
+repo-add instant.db.tar.gz *.pkg.tar.zst
